de544a5f34aa2b5a28d41882564a6ca35e787255
BetaGo <gouwantong@live.com>
2020-03-19 13:41:35 +0000

    fixK type error

--- src/app.module.ts
+++ src/app.module.ts
@@ -45,7 +45,7 @@ import { JwtModule } from '@nestjs/jwt';
       ): Promise<MysqlConnectionOptions> => ({
         type: 'mysql',
         host: configService.get('DATABASE_HOST'),
-        port: parseInt(configService.get('DATABASE_PORT'), 10),
+        port: configService.get('DATABASE_PORT'),
         username: configService.get('DATABASE_USER'),
         password: configService.get('DATABASE_PASSWORD'),
         database: configService.get('DATABASE_NAME'),
